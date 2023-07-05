477da6c3cde750d642f00548469fabed8610a789
aryamanprince <2019uee1722@mnit.ac.in>
2023-07-05 22:59:45 +0530

    test files and req.txt

--- app_config.py
+++ app_config.py
@@ -1,5 +1,5 @@
 ## config file for defenetions
 
-API_KEY = "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"
+API_KEY = "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"
 LIMIT_KEY = "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"
 FALSE_KEY = LIMIT_KEY+"i"
