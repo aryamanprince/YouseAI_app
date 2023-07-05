6309c213c3ef9c06a3a7d86bca0b350bcd2c6cab
aryamanprince <2019uee1722@mnit.ac.in>
2023-07-06 00:08:10 +0530

    some changes

--- app_config.py
+++ app_config.py
@@ -1,5 +1,5 @@
 ## config file for defenetions
 
-API_KEY = "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"
+API_KEY = "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"
 LIMIT_KEY = "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"
 FALSE_KEY = LIMIT_KEY+"i"
